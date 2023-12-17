bda1c0295dbcd426c6ee3f4280a4a46e607665d9
lbodlev888 <lbodlev888@gmail.com>
2023-12-18 00:32:17 +0200

    Bugs solved, multidigit numbers added

--- expresie.cpp
+++ expresie.cpp
@@ -11,22 +11,36 @@ unsigned prioritate(char op)
 	else return 2;
 }
 
+int conv(string s)
+{
+	int rez = 0;
+	for(int i = 0; i < s.length(); i++)
+		rez = rez * 10 + s[i] - '0';
+	return rez;
+}
+
 int main()
 {
 	string s = "(2+(4-6/2*3+1)+(2-4/(2-1)+1)-1)";// cin >> s;
-	if(s[0] != '(') {
-		s.insert(0, "(");
-		s += ")";
-	}
-	vector<char> ordine; stack<char> operatii;
+	s = "(" + s + ")";
+	vector<string> ordine; stack<char> operatii;
 	for(int i = 0; i < s.length(); i++)
 	{
-		if(isdigit(s[i])) ordine.push_back(s[i]);
+		if(isdigit(s[i])) {
+			string str = "";
+			str += s[i];
+			ordine.push_back(str);
+			while(isdigit(s[i+1]))
+			{
+				ordine[ordine.size()-1] += s[i+1];
+				i++;
+			}
+		}
 		else if(s[i] >= 42 && s[i] <= 47) {
 			unsigned current = prioritate(s[i]);
 			while(prioritate(operatii.top()) <= current && operatii.top() != '(')
 			{
-				ordine.push_back(operatii.top());
+				ordine.push_back("" + operatii.top());
 				operatii.pop();
 			}
 			operatii.push(s[i]);
@@ -34,23 +48,25 @@ int main()
 		else if(s[i] == ')') {
 			while(operatii.top() != '(')
 			{
-				ordine.push_back(operatii.top());
+				string str = "";
+				str += operatii.top();
+				ordine.push_back(str);
 				operatii.pop();
 			}
 			operatii.pop();
 		}
 		else operatii.push(s[i]);
 	}
-	stack<int> rezultate;
+	stack<double> rezultate;
 	for(int i = 0; i < ordine.size(); i++)
 	{
-		if(isdigit(ordine[i])) rezultate.push(ordine[i] - '0');
+		if(isdigit(ordine[i][0])) rezultate.push(conv(ordine[i]));
 		else {
 			int nr1 = rezultate.top();
 			rezultate.pop();
-			int nr2 = rezultate.top();
+			double nr2 = rezultate.top();
 			rezultate.pop();
-			switch(ordine[i])
+			switch(ordine[i][0])
 			{
 				case '+': nr2+=nr1; break;
 				case '-': nr2-=nr1; break;
